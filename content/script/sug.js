9132dedce08fcdbb7c691e8be3fcc4ce8d891150
Fatemeh <fatemehfaryadras7@gmail.com>
2023-04-11 16:44:45 +0330

    add my project

---
+++ content/script/sug.js
@@ -0,0 +1,36 @@
+let suggestions = [
+    "Custom Shoes",
+    "Sale Shoes",
+    "Soccer Cleats",
+    "All Clothing",
+    "Tops & T-Shirts",
+    "Shorts",
+    "Hoodies",
+    "Joggers",
+    "Sports Bras",
+    "Pants",
+    "Training Shoes",
+    "Yoga",
+    "nfant",
+    "Shoes",
+    "Basketball",
+    "Running",
+    "Jordan",
+    "Clothing",
+    "Backpacks",
+    "Socks",
+    "Sale",
+    "All Shoes",
+    "Jordan Shoes",
+    "Running Shoes",
+    "Basketball Shoes",
+    "Tennis Shoes",
+];
+
+
+
+
+
+
+
+
